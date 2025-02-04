a362f25ab7d4bdbcbec0643ff3faa8b6783696aa
Asad Bukhari <asad.bukhari@bentley.com>
2025-02-04 11:28:52 +0500

    Based on Access Token call iTwin APIs

--- Authorization/NodeCLI/src/index.ts
+++ Authorization/NodeCLI/src/index.ts
@@ -1,6 +1,9 @@
 import { NodeCliAuthorizationClient } from "@itwin/node-cli-authorization";
+import * as readline from "readline";
+import axios from "axios";
 
 const authClient = new NodeCliAuthorizationClient({clientId: "[Add Client Id Here]", scope: "itwin-platform"});
+let accessToken: string = "";
 
  (async() => {
 
@@ -8,7 +11,60 @@ const authClient = new NodeCliAuthorizationClient({clientId: "[Add Client Id Her
 
     await authClient.signIn();  // Sign in to iTwin.js
 
-    console.log("Access Token: ", await authClient.getAccessToken());
+    accessToken = await authClient.getAccessToken();
+    
+    await promptUserForDetails();
 
     await authClient.signOut(); // Sign out of iTwin.js
 })();
+
+
+async function promptUserForDetails() {
+    const reader = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    const getDetails = async (option: number, guid: string) => {
+        switch (option) {
+            case 1:
+                console.log(`Extracting iModel details for GUID: ${guid}`);
+                await fetchDetails(`https://api.bentley.com/imodels/${guid}`);
+                break;
+            case 2:
+                console.log(`Extracting iTwin details for GUID: ${guid}`);
+                await fetchDetails(`https://api.bentley.com/imodels/?iTwinId=${guid}`);
+                break;
+            default:
+                console.log("Invalid option");
+        }
+    };
+
+    reader.question('Menu:\n1. Extract iModel Details based on Guid\n2. Extract iTwin Details based on Guid\n3. Quit\nChoose an option:\n', (option) => {
+        if (parseInt(option) === 3) {
+            console.log("Quitting");
+            reader.close();
+            return;
+        }
+        reader.question('Enter the GUID: ', async (guid) => {
+            await getDetails(parseInt(option), guid);
+            reader.close();
+        });
+    });
+}
+
+
+async function fetchDetails(url: string) {
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                Accept: "application/vnd.bentley.itwin-platform.v2+json",
+                Authorization: `${accessToken}`
+            }
+        });
+        console.log("API Response: ", response.data);
+    } catch (error) {
+        console.error("Error fetching details: ", error);
+    }
+}
+
