3ad7caa3e3ed53e300a28e568b6d09941266a4fb
Asad Bukhari <asad.bukhari@bentley.com>
2025-02-06 15:05:09 +0500

    Use Auth Token to fetch iModel Details

--- Authorization/SPA/src/App.tsx
+++ Authorization/SPA/src/App.tsx
@@ -1,8 +1,22 @@
 
+import React from "react";
+import { useAuth } from "./hooks/useAuth";
+import IModelDetails from "./IModelDetails";
+
+/**
+ * The main application component.
+ * 
+ * This component uses the `useAuth` hook to retrieve an access token and displays it if available.
+ * 
+ * @component
+ * @returns {React.FC} The App component.
+ */
+
 const App: React.FC = () => {
+  const accessToken = useAuth();
 
   return (
-    <>iTwin.js + React + Typescript</>
+    <>{accessToken && <IModelDetails />}</>
   );
 };
 
